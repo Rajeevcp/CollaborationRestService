8ec19967741bb14fcd08f2b26f49259d5a45ba0c
Rajeev <rajeevcp92@gmail.com>
2017-05-12 09:14:46 +0530

    Add collaboration RestService

---
+++ src/main/java/com/niit/controller/BlogController.java
@@ -0,0 +1,135 @@
+package com.niit.controller;
+
+import java.util.List;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.collaboration.dao.BlogDAO;
+import com.niit.collaboration.model.Blog;
+
+@RestController
+public class BlogController {
+
+	@Autowired
+	private Blog blog;
+
+	@Autowired
+	private BlogDAO blogDAO;
+
+	@GetMapping("helloblog")
+	public String blogHelloUser() {
+		return "Hello Blog Controller";
+	}
+
+	@RequestMapping("blogs")
+	public ResponseEntity<List<Blog>> getAllBlog() {
+
+		List<Blog> blogList = blogDAO.list();
+
+		return new ResponseEntity<List<Blog>>(blogList, HttpStatus.OK);
+	}
+
+	@GetMapping("blog/{id}")
+	public ResponseEntity<Blog> getBlogById(@PathVariable("id") String id) {
+		blog = blogDAO.getBlogById(id);
+
+		if (blog == null) {
+			blog = new Blog();
+			blog.setErrorCode("404");
+			blog.setErrorMessage("Blog doesn't exist with this id " + id);
+		} else {
+			blog.setErrorCode("200");
+			blog.setErrorMessage("Success");
+		}
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "blog/create", method = RequestMethod.POST)
+	public ResponseEntity<Blog> createBlog(@RequestBody Blog blog) {
+
+		if (blogDAO.getBlogById(blog.getId()) == null) {
+			if (blogDAO.save(blog) == true) {
+				blog.setErrorCode("200");
+				blog.setErrorMessage("Blog Created");
+			} else {
+				blog.setErrorCode("404");
+				blog.setErrorMessage("Failed to create Blog");
+			}
+			return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+		}
+		blog.setErrorCode("404");
+		blog.setErrorMessage("Blog exist with this id " + blog.getId());
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "blog/update", method = RequestMethod.PUT)
+	public ResponseEntity<Blog> updateBlog(@RequestBody Blog blog) {
+
+		if (blogDAO.getBlogById(blog.getId()) != null) {
+			if (blogDAO.update(blog) == true) {
+				blog.setErrorCode("200");
+				blog.setErrorMessage("Blog Updated Successfully");
+			} else {
+				blog.setErrorCode("404");
+				blog.setErrorMessage("Blog Updated Failed");
+			}
+			return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+		}
+		blog.setErrorCode("404");
+		blog.setErrorMessage("No blog Exist with id " + blog.getId());
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+	@GetMapping("blog/accept/{id}")
+	public ResponseEntity<Blog> acceptBlog(@PathVariable("id") String id) {
+		if (blogDAO.getBlogById(id) != null) {
+			blog = blogDAO.getBlogById(id);
+			blog.setStatus('Y');
+			if (blogDAO.update(blog) == true) {
+				blog.setErrorCode("200");
+				blog.setErrorMessage("Blog accepted");
+			} else {
+				blog.setErrorCode("404");
+				blog.setErrorMessage("Request Failed");
+			}
+			return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+		}
+		blog.setErrorCode("404");
+		blog.setErrorMessage("Blog not exist with this id " + id);
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+	}
+
+	@GetMapping("blog/reject/{id}")
+	public ResponseEntity<Blog> rejectBlog(@PathVariable("id") String id) {
+		if (blogDAO.getBlogById(id) != null) {
+			blog = blogDAO.getBlogById(id);
+			blog.setStatus('N');
+			if (blogDAO.update(blog) == true) {
+				blog.setErrorCode("200");
+				blog.setErrorMessage("Rejected");
+			} else {
+				blog.setErrorCode("404");
+				blog.setErrorMessage("Rejection Failed");
+			}
+			return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+		}
+		blog.setErrorCode("404");
+		blog.setErrorMessage("Blog doesn't exist with id " + id);
+		return new ResponseEntity<Blog>(blog, HttpStatus.OK);
+
+	}
+
+}
---
+++ src/main/java/com/niit/controller/UserController.java
@@ -0,0 +1,123 @@
+package com.niit.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.collaboration.dao.UserDAO;
+import com.niit.collaboration.model.User;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private User user;
+
+	@Autowired
+	private UserDAO userDAO;
+
+	@Autowired
+	private HttpSession session;
+	
+	@GetMapping("/hello")
+	public String printWelcome() {
+		System.out.println("Started");
+		// ModelAndView mv = new ModelAndView("/foo");
+		user = (User) session.getAttribute("user");
+		return "Hellosssssssss"+user.getId();
+
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> getAllUser() {
+		List<User> userList = userDAO.list();
+		user = (User) session.getAttribute("user");
+		System.out.println("given id is "+user.getId());
+		return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
+	}
+
+	@GetMapping("/user/{id}")
+	public ResponseEntity<User> getUserByID(@PathVariable("id") String id) {
+
+		user = userDAO.get(id);
+
+		if (user == null) {
+			user = new User();
+			user.setErrorCode("404");
+			user.setErrorMessage("User does not exist with the id :" + id);
+		} else {
+			user.setErrorCode("200");
+			user.setErrorMessage("success");
+		}
+
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "/user/update/")
+	public ResponseEntity<User> updateUser(@RequestBody User user) {
+
+		if (userDAO.get(user.getId()) == null) {
+
+			user = new User(); // ?
+			user.setErrorCode("404");
+			user.setErrorMessage("User does not exist with id " + user.getId());
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		}
+
+		userDAO.update(user);
+
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/validate/", method = RequestMethod.POST)
+	public ResponseEntity<User> validateUser(@RequestBody User user) {
+		user = userDAO.isValidCredentials(user.getId(), user.getPassword());
+		if (user != null) {
+			user.setErrorCode("200");
+			user.setErrorMessage("You have successfully logged in.");
+			session.setAttribute("user", user);
+		} else {
+			user = new User(); // Do wee need to create new user?
+			user.setErrorCode("404");
+			user.setErrorMessage("Invalid Credentials.  Please enter valid credentials");
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/", method = RequestMethod.POST)
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+
+		if (userDAO.get(user.getId()) == null) {
+
+			if (userDAO.save(user) == true) {
+				user.setErrorCode("200");
+				user.setErrorMessage(
+						"Thank you  for registration. You have successfully registered as " + user.getRole());
+			} else {
+				user.setErrorCode("404");
+				user.setErrorMessage("Could not complete the operatin please contact Admin");
+
+			}
+
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		}
+
+		user.setErrorCode("404");
+		user.setErrorMessage("User already exist with id : " + user.getId());
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+}
